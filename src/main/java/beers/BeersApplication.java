69a46560688eb7a9a69a79b41311f3cbe5873f82
wankes2000 <wankes2000@gmail.com>
2015-09-26 19:29:10 +0200

    Spring Boot

---
+++ src/main/java/beers/BeersApplication.java
@@ -0,0 +1,12 @@
+package beers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeersApplication.class, args);
+    }
+}
