9bba63d868a725ad36e49d384cb8c238ed011350
wankes2000 <wankes2000@gmail.com>
2015-09-27 20:50:32 +0200

    Cambio en el script de arranque

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 sudo touch /var/log/mad_library.log
 sudo chmod 777 /var/log/mad_library.log
-sudo -c nohup java -jar /opt/webapps/mad_library.jar  >> /var/log/mad_library.log 2>&1 &
+sudo -c nohup java -jar /opt/webapps/mad_library.war  >> /var/log/mad_library.log 2>&1 &
