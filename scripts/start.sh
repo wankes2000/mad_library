31d9fc1968bfb9f2a15caa8e628c7b472ad0f0b4
wankes2000 <wankes2000@gmail.com>
2015-09-27 20:28:00 +0200

    Cambio en el script de arranque

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,3 @@
-sudo ./opt/webapps/spring_boot.sh start
+sudo touch /var/log/mad_library.log
+sudo chmod 777 /var/log/mad_library.log
+sudo -c nohup java -jar /opt/webapps/mad_library.jar  >> /var/log/mad_library.log 2>&1 &
